4c8d28e3510771407c519d1ec7c46f413656d813
Ighodaro Uyioghosa <uyibis@gmail.com>
2023-10-02 07:17:45 +0000

    Chat app

---
+++ README.md
@@ -0,0 +1,28 @@
+# Laravel Realtime Chat Application with Firebase
+
+
+<video width="630" height="300" src="https://youtu.be/jLiITqoBVqw"></video>
+
+This is a realtime chat application built using Laravel and Firebase. 
+## Features
+
+- Realtime Chat with Firebase
+- User-to-User Messaging
+- Responsive Design
+
+## Prerequisites
+
+Before you begin, ensure you have met the following requirements:
+
+- PHP (>= 7.3)
+- Composer
+- Node.js and npm
+- Firebase Account (for real-time messaging)
+- Laravel development environment
+
+## Installation
+
+1. Clone the repository:
+
+   ```bash
+   https://github.com/uyibis/Chat.git
---
+++ resources/views/index.blade.php
@@ -0,0 +1,163 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="chat_window">
+    <div class="top_menu">
+        <div class="buttons">
+            <div class="button close"></div>
+            <div class="button minimize"></div>
+            <div class="button maximize"></div>
+        </div>
+        <div class="title">Chat</div>
+    </div>
+    <ul class="messages"></ul>
+    <div class="bottom_wrapper clearfix">
+        <div class="message_input_wrapper">
+            <input class="message_input" placeholder="Type your message here..." />
+        </div>
+        <div class="send_message">
+            <div class="icon"></div>
+            <div class="text">Send</div>
+        </div>
+    </div>
+</div>
+<div class="message_template">
+    <li class="message">
+        <div class="avatar"></div>
+        <div class="text_wrapper">
+            <div class="text"></div>
+        </div>
+    </li>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+<script type="module">
+    var name= prompt("name");
+    console.log(name);
+    // Import the functions you need from the SDKs you need
+    //import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+    import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-app.js";
+    import { getDatabase,set,ref,push,child, onValue,onChildAdded} from "https://cdnjs.cloudflare.com/ajax/libs/firebase/9.6.6/firebase-database.min.js";
+
+    // TODO: Add SDKs for Firebase products that you want to use
+    // https://firebase.google.com/docs/web/setup#available-libraries
+
+    // Your web app's Firebase configuration
+    const firebaseConfig = {
+        apiKey: "AIzaSyBoEjP7yzNwfwc8e98qYMLR0hWxcFp2RFk",
+        authDomain: "prochat-84254.firebaseapp.com",
+        projectId: "prochat-84254",
+        storageBucket: "prochat-84254.appspot.com",
+        messagingSenderId: "733204794414",
+        appId: "1:733204794414:web:80ebd2749ec5e034f863bf"
+    };
+
+    const app = initializeApp(firebaseConfig);
+
+    const database =getDatabase(app);// firebase.database();
+
+    // Initialize Firebase
+    //const app = initializeApp(firebaseConfig);
+
+
+
+    var Message;
+    Message = function (arg) {
+        this.text = arg.text, this.message_side = arg.message_side;
+        this.draw = function (_this) {
+            return function () {
+                var $message;
+                $message = $($('.message_template').clone().html());
+                $message.addClass(_this.message_side).find('.text').html(_this.text);
+                $('.messages').append($message);
+                return setTimeout(function () {
+                    return $message.addClass('appeared');
+                }, 0);
+            };
+        }(this);
+        return this;
+    };
+
+    var getMessageText, message_side, sendMessage;
+    message_side = 'right';
+    getMessageText = function () {
+        var $message_input;
+        $message_input = $('.message_input');
+        return $message_input.val();
+    };
+
+    sendMessage = function (text,message_side) {
+        var $messages, message;
+        if (text.trim() === '') {
+            return;
+        }
+        $('.message_input').val('');
+        $messages = $('.messages');
+       // message_side = message_side === 'left' ? 'right' : 'left';
+        message = new Message({
+            text: text,
+            message_side: message_side
+        });
+        message.draw();
+        return $messages.animate({ scrollTop: $messages.prop('scrollHeight') }, 300);
+    };
+
+    $('.send_message').click(function (e) {
+
+        const id=push(child(ref(database),"message")).key;
+        set(ref(database,"message"+'/id'+id),{
+            data: getMessageText(),
+            name:name
+        })
+        //return //sendMessage(getMessageText(),'left');
+    });
+
+
+    //send data to realtime database
+    $('.message_input').keyup(function (e) {
+        if (e.which === 13) {
+            set(ref(database,"message"+'/id'+id),{
+                data: getMessageText(),
+                name:name
+            })
+           // return sendMessage(getMessageText(),'left');
+        }
+    });
+
+
+    //receive data from realtime database
+    const msgref=ref(database,"message");
+    onChildAdded(msgref,(data)=>{
+        if(data.val().name!= name){
+            sendMessage(data.val().data,'left');
+        }else{
+            sendMessage(data.val().data,'right');
+        }
+    })
+
+
+
+
+
+
+
+            /*sendMessage('Hello Philip! :)');
+            setTimeout(function () {
+                return sendMessage('Hi Sandy! How are you?');
+            }, 1000);
+            return setTimeout(function () {
+                return sendMessage('I\'m fine, thank you!');
+            }, 2000);*/
+
+
+</script>
+</body>
+</html>
